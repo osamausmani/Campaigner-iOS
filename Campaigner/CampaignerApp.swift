230cb9e529afb03d1509c5f326193a00b154024b
Osama Usmani <osamausmani1996@hotmail.com>
2023-05-07 18:50:11 +0500

    Initial Commit

---
+++ Campaigner/CampaignerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampaignerApp.swift
+//  Campaigner
+//
+//  Created by Osama Usmani on 07/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct CampaignerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
